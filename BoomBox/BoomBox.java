9f6ced7fae1c3c656a0697a5d2ac9d664d913437
DariaKuzina <dariakuz@mail.ru>
2016-02-01 19:20:02 +0300

    Resource leak fixed
    1.Resourse leak with I/O streams fixed
    2.Number of "magic constants" was reduced
    3.Comments were added

--- BoomBox/BoomBox.java
+++ BoomBox/BoomBox.java
@@ -9,6 +9,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+
 /**
  * MIDI-sequence creator. Use checkboxes to choose active instruments on every tick.
  * The idea and the biggest part of code are taken from "Head First Java" by Kathy Sierra and Bert Bates
@@ -20,14 +21,21 @@ public class BoomBox {
 	Sequence mySequence;
 	Track myTrack;
 	JFrame myFrame;
+	private final int noteStrenght=100,	defaultTempo=120, numberOfInstruments=16, numberOfTicks=16;
+	private final double tempoUpCoef=1.05,tempoDownCoef=0.95;
 	String[]instrumentNames={"Bass Drum","Closed Hi-Hat","Open Hi-Hat","Acoustic Snare","Crash Cymbal","Hand Clap","High Tom","Hi Bongo","Maracas","Whistle",
 			"Low Conga","Cowbell","Vibraslap","Low-mid Tom","High Agogo","Open Hi Conga"};
 	int[]instruments={35,42,46,38,49,39,50,60,70,72,64,56,68,47,67,63};
 	public static void main(String[] args) {
-		new BoomBox().buildGUI();
+		new BoomBox().start();
+	}
+	/**Builds GUI and makes initial settings*/
+	public void start(){
+		buildGUI();
+		SetUpMidi();
 	}
 
-public void buildGUI(){	
+private void buildGUI(){	
 	myFrame=new JFrame("Cyber BoomBox");
 	myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	BorderLayout layout=new BorderLayout();
@@ -79,45 +87,44 @@ public void buildGUI(){
 	background.add(BorderLayout.WEST,nameBox);
 	myFrame.getContentPane().add(background);
 	
-	GridLayout grid=new GridLayout(16, 16);
+	GridLayout grid=new GridLayout(numberOfInstruments, numberOfInstruments);
 	grid.setHgap(2);
 	grid.setVgap(1);
 	mainPanel=new JPanel(grid);
 	background.add(BorderLayout.CENTER,mainPanel);
 	
-	for(int i=0;i<256;i++){
+	for(int i=0;i<numberOfInstruments*numberOfTicks;i++){
 		JCheckBox c =new JCheckBox();
 		c.setSelected(false);
 		checkboxList.add(c);
 		mainPanel.add(c);
-	}
-	SetUpMidi();
-	
+	}	
 	myFrame.setBounds(50,50,300,300);
 	myFrame.pack();
 	myFrame.setVisible(true);
-}//end of buildGUI;
-
-public void SetUpMidi(){
+}
+/**Creates sequencer and makes initial settings*/
+private void SetUpMidi(){
 	try{
 		mySequencer=MidiSystem.getSequencer();
 		mySequencer.open();
 		mySequence=new Sequence(Sequence.PPQ, 4);
 		myTrack=mySequence.createTrack();
-		mySequencer.setTempoInBPM(120);
+		mySequencer.setTempoInBPM(defaultTempo);
 	}
 	catch(Exception e){e.printStackTrace();}
-}//end of SetUpMidi
-public void buildTrackAndStart(){
+}
+/**Creates track using information from checkboxes*/
+private void buildTrackAndStart(){
 	int[] trackList=null;
 	mySequence.deleteTrack(myTrack);
 	myTrack=mySequence.createTrack();
 	int key;
-	for(int i=0;i<16;i++){
-		trackList=new int[16];
+	for(int i=0;i<numberOfInstruments;i++){
+		trackList=new int[numberOfTicks];
 		key=instruments[i];
-		for(int j=0;j<16;j++){
-			JCheckBox jc=(JCheckBox)checkboxList.get(j+(16*i));
+		for(int j=0;j<numberOfTicks;j++){
+			JCheckBox jc=(JCheckBox)checkboxList.get(j+(numberOfTicks*i));
 			if(jc.isSelected()){
 				trackList[j]=key;
 			}
@@ -126,36 +133,39 @@ public void buildTrackAndStart(){
 			}
 		}
 		makeTracks(trackList);
-		myTrack.add(makeEvent(ShortMessage.CONTROL_CHANGE, 1, 127, 0,16));
+		myTrack.add(makeEvent(ShortMessage.CONTROL_CHANGE, 1, 0, 0,numberOfTicks));
 	}
-	myTrack.add(makeEvent(ShortMessage.PROGRAM_CHANGE, 9, 1, 0,15));
+	myTrack.add(makeEvent(ShortMessage.PROGRAM_CHANGE, 9, 1, 0,numberOfTicks-1));
 	try{
 		mySequencer.setSequence(mySequence);
-		mySequencer.setLoopCount(mySequencer.LOOP_CONTINUOUSLY);
+		mySequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
 		mySequencer.start();
-		mySequencer.setTempoInBPM(120);
+		mySequencer.setTempoInBPM(defaultTempo);
 	}
 	catch(Exception e){
 		e.printStackTrace();
 	}
 	
-}//end of buildTrackAndStart
-public void makeTracks(int[]list){
-	for(int i=0;i<16;i++){
-		int key=list[i];
+}
+/**Makes track for instrument
+ * @param list Contains instrument number in the i-th place, if it should play on the i-th tick and 0 otherwise*/
+private void makeTracks(int[]list){
+	int key;
+	for(int i=0;i<numberOfTicks;i++){
+		key=list[i];
 		if(key!=0){
-			myTrack.add(makeEvent(ShortMessage.NOTE_ON, 9, key, 100,i));
-			myTrack.add(makeEvent(ShortMessage.NOTE_OFF, 9, key, 100,i+1));
+			myTrack.add(makeEvent(ShortMessage.NOTE_ON, 9, key, noteStrenght,i));
+			myTrack.add(makeEvent(ShortMessage.NOTE_OFF, 9, key, noteStrenght,i+1));
 		}
 	}
-}//end of makeTracks
-/**Creates new midi event
+}
+/**Creates new MIDI  event
  * @param comd MIDI command
  * @param chan channel
  * @param one first data byte
  * @param two second data byte
  * @param tick time-stamp*/
-public MidiEvent makeEvent(int comd,int chan,int one,int two,int tick){
+private MidiEvent makeEvent(int comd,int chan,int one,int two,int tick){
 	MidiEvent event=null;
 	try{
 		ShortMessage a=new ShortMessage();
@@ -166,10 +176,12 @@ public MidiEvent makeEvent(int comd,int chan,int one,int two,int tick){
 		e.printStackTrace();
 	}
 	return event;
-}//end of makeEvent
+}
+/**Writes scheme as boolean array in file
+ * @param file Output file*/
 private void saveInFile(File file){
-	boolean[]checkboxState=new boolean[256];
-	for (int i=0;i<256;i++) {
+	boolean[]checkboxState=new boolean[numberOfInstruments*numberOfTicks];
+	for (int i=0;i<numberOfInstruments*numberOfTicks;i++) {
 		JCheckBox check=checkboxList.get(i);
 		if(check.isSelected()){	
 			checkboxState[i]=true;
@@ -178,22 +190,32 @@ private void saveInFile(File file){
 	try{
 		FileOutputStream fileStream=new FileOutputStream(file);
 		ObjectOutputStream os=new ObjectOutputStream(fileStream);
-		os.writeObject(checkboxState);
+		try{
+			os.writeObject(checkboxState);			
+		}finally{
+			os.close();
+		}	
 		
 	}catch(Exception e){
-		e.printStackTrace();
+		JOptionPane.showMessageDialog(myFrame, "Can't save in this file");
 	}
-}//end of SaveInFile
+}
+/**Reads scheme from file
+ * @param file Input file*/
 private void restoreFromFile(File file){
 	boolean[]checkboxState=null;
 	try{
 		FileInputStream fileIn=new FileInputStream(file);
 		ObjectInputStream iStream=new ObjectInputStream(fileIn);
-		checkboxState=(boolean[])iStream.readObject();
+		try{
+			checkboxState=(boolean[])iStream.readObject();
+		}finally{
+			iStream.close();
+		}
 	}catch (Exception e){
-		e.printStackTrace();
+		JOptionPane.showMessageDialog(myFrame, "Can't read from this file");
 	}
-	for (int i=0;i<256;i++) {
+	for (int i=0;i<numberOfInstruments*numberOfTicks;i++) {
 		JCheckBox check=checkboxList.get(i);
 		if(checkboxState[i]){	
 			check.setSelected(true);
@@ -203,48 +225,61 @@ private void restoreFromFile(File file){
 		}
 	}
 	mySequencer.stop();
-}//end of restoreFromFile
+}
+/**Listener for start button*/
 class MyStartListener implements ActionListener{
+	@Override
 	public void actionPerformed(ActionEvent a){
 		buildTrackAndStart();
 	}
-}//end of MyStartListener
+}
+/**Listener for stop button*/
 class MyStopListener implements ActionListener{
+	@Override
 	public void actionPerformed(ActionEvent a){
 		mySequencer.stop();
 	}
-}//end of MyStopListener
+}
+/**Listener for upTempo button*/
 class MyUpTempoListener implements ActionListener{
+	@Override
 	public void actionPerformed(ActionEvent a){
 		float tempoFactor=mySequencer.getTempoFactor();
-		mySequencer.setTempoFactor((float)(tempoFactor*1.03));
+		mySequencer.setTempoFactor((float)(tempoFactor*tempoUpCoef));
 	}
-}//end of MyUpTempoListener
+}
+/**Listener for downTempo button*/
 class MyDownTempoListener implements ActionListener{
+	@Override
 	public void actionPerformed(ActionEvent a){
 		float tempoFactor=mySequencer.getTempoFactor();
-		mySequencer.setTempoFactor((float)(tempoFactor*0.97));
+		mySequencer.setTempoFactor((float)(tempoFactor*tempoDownCoef));
 	}
-}//end of MyDownTempoListener
+}
+/**Listener for clearBoxes button*/
 class MyClearBoxesListener implements ActionListener{
+	@Override
 	public void actionPerformed(ActionEvent a){
 		for(JCheckBox box:checkboxList){
 			box.setSelected(false);
 		}
 		mySequencer.stop();
 	}
-}//end of MyClearBoxesListener
+}
+/**Listener for "Load file" menu item*/
 class MyLoadFileListener implements ActionListener{
+	@Override
 	public void actionPerformed(ActionEvent a){
 			JFileChooser fileIn=new JFileChooser();
 			int res=fileIn.showOpenDialog(myFrame);
 			if(res==JFileChooser.APPROVE_OPTION){		
 			restoreFromFile(fileIn.getSelectedFile());
-			}
-			
+			}			
 }
-}//end of MyLoadFileListener
-class MySaveFileListener implements ActionListener{
+}
+/**Listener for "Save file" menu item*/
+private class MySaveFileListener implements ActionListener{
+	@Override
 	public void actionPerformed(ActionEvent a){
 		JFileChooser fileSave=new JFileChooser();
 		int res=fileSave.showSaveDialog(myFrame);
@@ -252,6 +287,9 @@ class MySaveFileListener implements ActionListener{
 		saveInFile(fileSave.getSelectedFile());
 		}
 		
+		}
 	}
-}//end of MySaveFileListener
-}//end of BoomBox
+
+}
+
+
